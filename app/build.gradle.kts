4d389a71c11f7d68fe04b4f1acf96730b5bb0b9e
Naveen <snaveen935@gmail.com>
2023-10-03 19:55:02 +0530

    Add debug build type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,13 @@ android {
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
         release {
             isMinifyEnabled = true
             proguardFiles(
